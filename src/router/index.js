3bef7b00b95b8f11ee8aecf157ecf06749d530ac
James Baskerville <jameswbaskerville@gmail.com>
2020-10-26 22:11:39 -0700

    fix: Fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/site',
+    path: '/',
     component: Site,
     children: [
       {
@@ -37,7 +37,7 @@ const routes = [
         name: 'Music',
         component: Music
       }
-    ],
+    ]
   },
   {
     path: '/trivia',
